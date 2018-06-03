724476ca85848c46d846b4ee8be554e1d7f60245
Ricardo Koller <kollerr@us.ibm.com>
2018-06-03 13:38:03 -0400

    using pexpect

--- syscall-dashboard/app.py
+++ syscall-dashboard/app.py
@@ -4,6 +4,7 @@ import subprocess
 import json
 import os
 import signal
+from pexpect.popen_spawn import PopenSpawn
 
 cl = []
 
@@ -20,32 +21,33 @@ class SocketHandler(websocket.WebSocketHandler):
 
     def open(self):
         if self not in cl:
-	    #dashNabla = pexpect.spawn('sudo stdbuf --output=0 sysdig proc.name=ukvm-bin -c countsc | python dash_histo.py --port 8050')
-	    #dashNabla = pexpect.spawn('sysdig proc.name=ukvm-bin -c countsc | python dash_histo.py --port 8050')
-	    logs = open('out-file.txt', 'w')
-	    #cmd = "sudo stdbuf --output=0 sysdig proc.name=ukvm-bin -c countsc | python dash_histo.py --port 8050"
-	    cmd = "stdbuf --output=0 sysdig proc.name=ukvm-bin -c countsc | python dash_histo.py --port 8050"
-	    #ps = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=logs, preexec_fn=os.setsid)
-	    ps = subprocess.Popen(cmd,shell=True,stdout=logs,stderr=logs, preexec_fn=os.setsid)
-	    data = {
-		'replNabla':'http://172.17.0.2:8081/',
-		'replProc':'http://localhost:8081/',
-		'dashNabla':'http://localhost:8050/',
-		'dashProc':'http://localhost:8051/'}
-	    data_json = json.dumps(data)
-	    self.write_message(data_json)
+            #dashNabla = pexpect.spawn('sysdig proc.name=ukvm-bin -c countsc | python dash_histo.py --port 8050')
+            logs = open('out-file.txt', 'w')
+            cmd = "stdbuf --output=0 sysdig proc.name=ukvm-bin -c countsc | python dash_histo.py --port 8050"
+            #ps = subprocess.Popen(cmd,shell=True,stdout=logs,stderr=logs, preexec_fn=os.setsid)
+            #ps = pexpect.popen_spawn.PopenSpawn(cmd, logfile=logs, preexec_fn=os.setsid)
+            ps = pexpect.spawn('/bin/bash', ['-c', cmd])
+            ps.expect("Running on http")
+            data = {
+                'replNabla':'http://172.17.0.2:8081/',
+                'replProc':'http://localhost:8081/',
+                'dashNabla':'http://localhost:8050/',
+                'dashProc':'http://localhost:8051/'}
+            data_json = json.dumps(data)
+            self.write_message(data_json)
             cl.append(self)
-	    procs[self] = ps
-            print 'new connection'
-	    print procs[self]
+            procs[self] = ps
+            print('new connection')
+            print(procs[self])
 
     def on_close(self):
         if self in cl:
-            print 'closed connection'
+            print('closed connection')
             cl.remove(self)
-	    ps = procs[self]
-	    print procs[self]
-	    os.killpg(os.getpgid(ps.pid), signal.SIGTERM)  # Send the signal to all the process groups
+            ps = procs[self]
+            print(procs[self])
+            #os.killpg(os.getpgid(ps.pid), signal.SIGTERM)  # Send the signal to all the process groups
+            os.killpg(os.getpgid(ps.pid), signal.SIGTERM)  # Send the signal to all the process groups
 
 app = web.Application([
     (r'/', IndexHandler),
@@ -55,7 +57,7 @@ app = web.Application([
 if __name__ == '__main__':
     app.listen(8888)
     try:
-	    ioloop.IOLoop.instance().start()
+            ioloop.IOLoop.instance().start()
     finally:
-	    for ps in procs.values():
-	    	os.killpg(os.getpgid(ps.pid), signal.SIGTERM)  # Send the signal to all the process groups
+            for ps in procs.values():
+                    os.killpg(os.getpgid(ps.pid), signal.SIGTERM)  # Send the signal to all the process groups
