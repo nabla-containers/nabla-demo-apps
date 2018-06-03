f15d2ab446e5b449a0a06682e20b6724cc088b70
Ricardo Koller <kollerr@us.ibm.com>
2018-06-03 03:05:27 -0400

    nits

--- syscall-dashboard/app.py
+++ syscall-dashboard/app.py
@@ -22,28 +22,9 @@ class SocketHandler(websocket.WebSocketHandler):
             print 'closed connection'
             cl.remove(self)
 
-class ApiHandler(web.RequestHandler):
-
-    @web.asynchronous
-    def get(self, *args):
-        self.finish()
-        id = self.get_argument("id")
-        value = self.get_argument("value")
-        data = {"id": id, "value" : value}
-        data = json.dumps(data)
-        for c in cl:
-            c.write_message(data)
-
-    @web.asynchronous
-    def post(self):
-        pass
-
 app = web.Application([
     (r'/', IndexHandler),
     (r'/ws', SocketHandler),
-    (r'/api', ApiHandler),
-    (r'/(favicon.ico)', web.StaticFileHandler, {'path': '../'}),
-    (r'/(rest_api_example.png)', web.StaticFileHandler, {'path': './'}),
 ])
 
 if __name__ == '__main__':
